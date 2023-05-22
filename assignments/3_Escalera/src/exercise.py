8889019d8cc71d128b556cc8fb75975238b00ff7
ShaddaiRuiz <89268693+ShaddaiRuiz@users.noreply.github.com>
2023-05-21 21:25:24 -0600

    Update exercise.py

--- assignments/3_Escalera/src/exercise.py
+++ assignments/3_Escalera/src/exercise.py
@@ -1,5 +1,9 @@
+import math
 def main():
-    pass
+    altura = float(input('Escribe la altura de la casa: '))
+    angulo = int(input('Escribe el angulo en grados: '))
+    a = altura / math.sin(math.radians(angulo))
+    print ('Largo de la escalera: ', round(a))
 
 if __name__ == '__main__':
     main()
